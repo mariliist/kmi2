618d374cc42056279eeca1902ce630e46cb07d4f
Mariliis Toon <mariliistoon@gmail.com>
2023-11-22 11:43:55 +0200

    index ja css add

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
-const { response } = require('express');
 const express = require('express');
-const { request } = require('http');
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
 
-
-app.get('/', (request, response)=>{
-response.sendFile(__dirname+'\\index.html');
+app.get('/', (request, response) => {
+    response.render('index', {resultToRender: ''});
 });
 
 
 app.post('/bmi', (request, response) => {
-let height = Number(request.body.height)/100;
-let weight = Number(request.body.weight);
-let bmi = weight/(height*height);
-console.log(bmi);
+    let height = Number(request.body.height) / 100;
+    let weight = Number(request.body.weight);
+    let bmi = (weight / (height * height)).toFixed(2);
+    let userResult = '';
+
+    if (bmi < 19) {
+        userResult = 'alakaal';
+    }
+    else if (bmi >= 19 && bmi <= 24.9) {
+        userResult = 'normaalkaal';
+    }
+    else if (bmi >= 25 && bmi <= 29.9) {
+        userResult = 'ülekaal';
+    }
+    else {
+        userResult = 'rasvumine';
+    }
+
+    let result = {
+        userBMI: bmi,
+        result: userResult
+    };
+    response.render('index', {resultToRender: result});
+
 });
 
 
 const port = 3000
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Server is running ${port}.`);
 });
